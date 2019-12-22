a1ca6db746a83fa6a06601e72b85d0aa2ada7605
Aashish Kushwaha <aashishk1997@gmail.com>
2019-12-22 20:14:40 +0530

    socket setup__1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+// import socket from 'socket.io-client';
+import io from '../node_modules/socket.io-client/dist/socket.io.js';
+//'../node_modules/socket.io-client';
+//'socket.io/node_modules/socket.io-client';
 import './App.css';
 
 function App() {
+  const socket = io('wss://api.delta.exchange:2096');
+  console.log(socket);
   return (
     <div className="App">
       <h1> Proximity challenge  </h1>
     </div>
+
   );
 }
 
